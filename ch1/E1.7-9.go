07c119bd0db02b85592f41a81e26874583472408
wey46 <wyu0618@gmail.com>
2019-01-22 13:38:23 -0500

    fetch

---
+++ ch1/E1.7-9.go
@@ -0,0 +1,31 @@
+// Excercise 1.7: use io.Copy(dst, src)
+// Excercise 1.8: Modify fetch to add the prefix http://
+// to each argument URL if it is missing.
+// Excercise 1.9: also print the HTTP status code, found in resp.Status
+// Fetch prints the content found at each specified URL.
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		_, err = io.Copy(os.Stdout, resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+	}
+}
+
+//!-
